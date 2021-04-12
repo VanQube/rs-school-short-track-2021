e225f1b880063e036c7fd6a7639487ea60b1a52d
3xtZzy <38046904+VanQube@users.noreply.github.com>
2021-04-12 22:50:20 +0300

    Update 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,34 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') {
+    return '';
+  }
+  let counter = 1;
+  let encdStr = '';
+  let currChar = str[0];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === currChar) {
+      counter++;
+    } else {
+      if (counter === 1) {
+        encdStr += currChar;
+      } else {
+        encdStr += counter + currChar;
+      }
+      currChar = str[i];
+      counter = 1;
+    }
+    if (i === str.length - 1) {
+      if (counter === 1) {
+        encdStr += currChar;
+      } else {
+        encdStr += counter + currChar;
+      }
+    }
+  }
+  return encdStr;
 }
 
 module.exports = encodeLine;
