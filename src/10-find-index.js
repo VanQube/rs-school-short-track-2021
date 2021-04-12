6fa4f0b4ed747af4a6d93ca175a5f6720ffc0ae7
3xtZzy <38046904+VanQube@users.noreply.github.com>
2021-04-12 23:49:41 +0300

    Update 10-find-index.js

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -12,7 +12,20 @@
  *
  */
 function findIndex(array, value) {
-  return array.indexOf(value);
+  let start = 0;
+  let end = array.length - 1;
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+    if (value === array[middle]) {
+      return middle;
+    }
+    if (value < array[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;
